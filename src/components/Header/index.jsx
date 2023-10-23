b4af77ee317faa4281e2b6b51af52b0baa6ea433
Abdulqodir <abdulqodirmaxmadaliyev03@gmail.com>
2023-10-23 23:59:11 +0500

    initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <header className='main-header'>
+            <nav className='main-nav'>
+                <img src='/src/assets/Logo.png' width={139} height={64.394} alt='logo' />
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
